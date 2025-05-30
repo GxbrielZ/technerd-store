df549acfe7b136d7b6449b36c6dd7ed7d5d502ce
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 20:37:05 +0200

    Add data with new articles

---
+++ src/data/newArticles.js
@@ -0,0 +1,27 @@
+import cameramanImage from "../assets/articles/cameraman.jpg";
+import navigationImage from "../assets/articles/navigation.jpg";
+import gamingRoomImage from "../assets/articles/gamingRoom.jpg";
+
+export const newArticles = [
+  {
+    id: 1,
+    title: "Dla pasjonatów fotografii",
+    description:
+      "Sprawdź naszą ofertę sprzętu fotograficznego w super cenach i już dziś zacznij uwieczniać chwile!",
+    image: cameramanImage,
+  },
+  {
+    id: 2,
+    title: "Wyjeżdżasz w podróż?",
+    description:
+      "Zaopatrz się w najlepszej jakości sprzęt nawigacyjny i zawsze trafiaj do celu bez problemu.",
+    image: navigationImage,
+  },
+  {
+    id: 3,
+    title: "Zbuduj wymarzone stanowisko",
+    description:
+      "Marzy Ci się stanowisko na miarę prawdziwego gamera? Z naszą pomocą to nie będzie problem!",
+    image: gamingRoomImage,
+  },
+];
