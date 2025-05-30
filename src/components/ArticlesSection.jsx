a7e99738d6b2e3f16ccfa90fa68c5690ecafeb6b
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 20:35:28 +0200

    Change font color

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -13,7 +13,7 @@ const ArticlesSection = ({ articles }) => {
               className="w-full h-80 md:h-[35rem] object-cover"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-80"></div> {/* Ciemniejszy gradient */}
-            <div className="absolute bottom-0 left-0 right-0 p-4 md:px-8 md:py-8 text-white">
+            <div className="absolute bottom-0 left-0 right-0 p-4 md:px-8 md:py-8 text-darkwhite">
               <h2 className="text-xl md:text-3xl font-bold mb-2">
                 {article.title}
               </h2>
