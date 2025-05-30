4a5eeabaeb10ec49456776eb8d1f0eb8df74b964
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 14:24:23 +0200

    Add ProductsSection component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import Hero from "../components/Hero";
+import ProductsSection from "../components/ProductsSection";
+import { newProducts } from "../data/newProducts";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <ProductsSection headerTitle="Nowe Produkty" products={newProducts} />
     </div>
   );
 };
