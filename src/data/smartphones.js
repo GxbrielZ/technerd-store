2c3febb5c5634c2b9898176a2b7264a289ad09d2
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 19:54:24 +0200

    Add image sources

--- src/data/smartphones.js
+++ src/data/smartphones.js
@@ -1,50 +1,59 @@
+import product1 from "../assets/products/smartphones/samsung-galaxy-a26.jpg";
+import product2 from "../assets/products/smartphones/samsung-galaxy-s25-edge.jpg";
+import product3 from "../assets/products/smartphones/motorola-edge-60.jpg";
+import product4 from "../assets/products/smartphones/google-pixel-9-proxl.jpg";
+import product5 from "../assets/products/smartphones/motorola-razr-60-ultra.jpg";
+import product6 from "../assets/products/smartphones/realme-14-pro.jpg";
+import product7 from "../assets/products/smartphones/xiaomi-poco-x7-pro.jpg";
+import product8 from "../assets/products/smartphones/nubia-focus-pro.jpg";
+
 export const smartphones = [
     {
         id: 1,
         name: 'Smartfon SAMSUNG Galaxy A26 6/128GB 6.7" Czarny',
         price: 1099.00,
-        image: "",
+        image: product1,
     },
     {
         id: 2,
         name: 'Smartfon SAMSUNG Galaxy S25 Edge 12/512GB 6.7" Czarny',
         price: 5399.00,
-        image: "",
+        image: product2,
     },
     {
         id: 3,
         name: 'Smartfon MOTOROLA Edge 60 12/512GB 6.67" Niebieski',
         price: 1999.99,
-        image: "",
+        image: product3,
     },
     {
         id: 4,
         name: 'Smartfon GOOGLE Pixel 9 Pro XL 16/128GB 6.8" Zielonoszary',
         price: 3999.00,
-        image: "",
+        image: product4,
     },
     {
         id: 5,
         name: 'Smartfon MOTOROLA Razr 60 Ultra 16/512GB 7.0" Brązowy',
         price: 5699.00,
-        image: "",
+        image: product5,
     },
     {
         id: 6,
         name: 'Smartfon REALME 14 Pro+ 12/512GB 6.83" Perłowy',
         price: 2299.00,
-        image: "",
+        image: product6,
     },
     {
         id: 7,
         name: 'Smartfon XIAOMI Poco X7 Pro 12/512GB 6.67" Czarny',
         price: 1585.50,
-        image: "",
+        image: product7,
     },
     {
         id: 8,
         name: 'Smartfon NUBIA Focus Pro 8/256GB 6.72" Czarny',
         price: 699.99,
-        image: "",
+        image: product8,
     },
 ];
