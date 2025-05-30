86dea5882d08e8537ef08f6b23b55d04c0ecb4b3
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 19:53:21 +0200

    Create data file for hobbies section

---
+++ src/data/hobbies.js
@@ -0,0 +1,51 @@
+import hobby1 from "../assets/hobbies/sport.jpg";
+import hobby2 from "../assets/hobbies/garden.jpg";
+import hobby3 from "../assets/hobbies/art.jpg";
+import hobby4 from "../assets/hobbies/music.jpg";
+import hobby5 from "../assets/hobbies/bike.jpg";
+import hobby6 from "../assets/hobbies/games.jpg";
+import hobby7 from "../assets/hobbies/movies.jpg";
+import hobby8 from "../assets/hobbies/toys.jpg";
+
+export const hobbies = [
+  {
+    id: 1,
+    name: 'Sport',
+    image: hobby1,
+  },
+  {
+    id: 2,
+    name: 'Ogród',
+    image: hobby2,
+  },
+  {
+    id: 3,
+    name: 'Sztuka',
+    image: hobby3,
+  },
+  {
+    id: 4,
+    name: 'Muzyka',
+    image: hobby4,
+  },
+  {
+    id: 5,
+    name: 'Rower',
+    image: hobby5,
+  },
+  {
+    id: 6,
+    name: 'Gry',
+    image: hobby6,
+  },
+  {
+    id: 7,
+    name: 'Filmy',
+    image: hobby7,
+  },
+  {
+    id: 8,
+    name: 'Zabawki',
+    image: hobby8,
+  },
+];
