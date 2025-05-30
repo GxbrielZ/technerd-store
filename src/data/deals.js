babfba93bdda90bb57439241a11aca3418a77b5b
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 18:38:37 +0200

    Add data for deals section

---
+++ src/data/deals.js
@@ -0,0 +1,51 @@
+import deal1 from "../assets/deals/discount-deal.png";
+import deal2 from "../assets/deals/bike-deal.webp";
+import deal3 from "../assets/deals/smartwatch-deal.webp";
+import deal4 from "../assets/deals/gaming-deal.webp";
+import deal5 from "../assets/deals/dryer-deal.png";
+import deal6 from "../assets/deals/iphone-deal.webp";
+import deal7 from "../assets/deals/tv-deal.webp";
+import deal8 from "../assets/deals/child-deal.webp";
+
+export const deals = [
+  {
+    id: 1,
+    name: '-30% na drugi droższy produkt',
+    image: deal1,
+  },
+  {
+    id: 2,
+    name: 'Rowery do -40%',
+    image: deal2,
+  },
+  {
+    id: 3,
+    name: 'Smartwatche w najlepszej cenie!',
+    image: deal3,
+  },
+  {
+    id: 4,
+    name: 'Gamingowe rabaty',
+    image: deal4,
+  },
+  {
+    id: 5,
+    name: 'Suszarki do ubrań taniej z kodem rabatowym',
+    image: deal5,
+  },
+  {
+    id: 6,
+    name: 'iPhone taniej z kodem rabatowym',
+    image: deal6,
+  },
+  {
+    id: 7,
+    name: 'Telewizory taniej z kodem rabatowym',
+    image: deal7,
+  },
+  {
+    id: 8,
+    name: 'Prezenty na Dzień Dziecka',
+    image: deal8,
+  },
+];
