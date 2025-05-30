3bd9bea9e5ccf7a2a339607ec3f4cdad6f0ed8b3
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 19:52:22 +0200

    Add data for articles section

---
+++ src/data/recommendedArticles.js
@@ -0,0 +1,19 @@
+import bikingImage from "../assets/articles/biking.jpg";
+import streamingImage from "../assets/articles/streaming.jpg";
+
+export const recommendedArticles = [
+  {
+    id: 1,
+    title: "W podróż rowerem",
+    description:
+      "Sprawdź naszą ofertę najlepszych rowerów na długie wycieczki. Znajdź odpowiednie akcesoria i nie daj się zaskoczyć podczas podróży.",
+    image: bikingImage,
+  },
+  {
+    id: 2,
+    title: "Sprzęt dla streamerów",
+    description:
+      "Przejrzyj naszą ofertę sprzętu dla streamerów. Najlepsza jakość oraz niezawodność pozwoli Ci na długie godziny rozgrywki.",
+    image: streamingImage,
+  },
+];
