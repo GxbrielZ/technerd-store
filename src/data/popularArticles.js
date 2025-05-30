b9f9b2f2008b4b9a48d0c87c50790e02b51be6b5
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 21:18:23 +0200

    Create data with popular articles

---
+++ src/data/popularArticles.js
@@ -0,0 +1,27 @@
+import installmentImage from "../assets/articles/installment.jpg";
+import scooterImage from "../assets/articles/scooter.jpg";
+import gamesImage from "../assets/articles/games.jpg";
+
+export const popularArticles = [
+  {
+    id: 1,
+    title: "Wniosek ratalny",
+    description:
+      "Sprawdź zanim wypełnisz wniosek ratalny. Wszystko co musisz wiedzieć zmajdziesz tutaj!",
+    image: installmentImage,
+  },
+  {
+    id: 2,
+    title: "Najlepsze hulajnogi",
+    description:
+      "Ranking najlepszych hulajnóg elektrycznych w roku 2025. Znajdź sprzęt, który spełni Twoje wymagania.",
+    image: scooterImage,
+  },
+  {
+    id: 3,
+    title: "Premiery gier 2025",
+    description:
+      "Premiery gier na rok 2025. Sprawdź co wychodzi, a na co warto poczekać.",
+    image: gamesImage,
+  },
+];
