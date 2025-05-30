539c010ba0d20d8a536a29867f3f7eb6aa74c28f
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 20:51:40 +0200

    Update image sources

--- src/data/gaming.js
+++ src/data/gaming.js
@@ -1,50 +1,59 @@
-export const smartphones = [
+import product1 from "../assets/products/gaming/xbox-series-x.jpg";
+import product2 from "../assets/products/gaming/ps5.jpg";
+import product3 from "../assets/products/gaming/doom.jpg";
+import product4 from "../assets/products/gaming/ac-shadows.jpg";
+import product5 from "../assets/products/gaming/steelseries-arctis-nova.jpg";
+import product6 from "../assets/products/gaming/redragon-zeus.jpg";
+import product7 from "../assets/products/gaming/pad-xbox.jpg";
+import product8 from "../assets/products/gaming/ps5-pad.jpg";
+
+export const gaming = [
     {
         id: 1,
         name: 'Konsola MICROSOFT XBOX Series X bez napędu Biały',
         price: 2500.00,
-        image: "",
+        image: product1,
     },
     {
         id: 2,
         name: 'Konsola SONY PlayStation 5 Digital Slim',
         price: 1799.00,
-        image: "",
+        image: product2,
     },
     {
         id: 3,
         name: 'Doom: The Dark Ages Edycja Standardowa Gra Xbox Series X',
         price: 310.99,
-        image: "",
+        image: product3,
     },
     {
         id: 4,
         name: 'Assassin\'s Creed Shadows - Edycja Limitowana Gra PS5',
         price: 299.99,
-        image: "",
+        image: product4,
     },
     {
         id: 5,
         name: 'Słuchawki STEELSERIES Arctis Nova 7 Dragon Edition Bezprzewodowe',
         price: 480.00,
-        image: "",
+        image: product5,
     },
     {
         id: 6,
         name: 'Słuchawki REDRAGON Zeus Lite H510-LT Nauszne',
         price: 129.99,
-        image: "",
+        image: product6,
     },
     {
         id: 7,
         name: 'Kontroler MICROSOFT bezprzewodowy Xbox Carbon Black',
         price: 220.50,
-        image: "",
+        image: product7,
     },
     {
         id: 8,
         name: 'Kontroler SONY DualSense Edge Biało-czarny',
         price: 829.99,
-        image: "",
+        image: product8,
     },
 ];
