8d02a647d46d0e7b7960e10032596608c5bb1819
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 19:53:52 +0200

    Update Home page with new components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,21 @@ import ProductsSection from "../components/ProductsSection";
 import { newProducts } from "../data/newProducts";
 import DealsSection from "../components/DealsSection";
 import { deals } from "../data/deals";
+import ArticlesSection from "../components/ArticlesSection";
+import { recommendedArticles } from "../data/recommendedArticles";
+import { smartphones } from "../data/smartphones";
+import HobbiesSection from "../components/HobbiesSection";
+import { hobbies } from "../data/hobbies";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <ProductsSection headerTitle="Nowe Produkty" products={newProducts} />
+      <ProductsSection headerTitle="Nowe produkty" products={newProducts} />
       <DealsSection headerTitle="Oferty i okazje na czasie" deals={deals} />
+      <ArticlesSection articles={recommendedArticles} />
+      <ProductsSection headerTitle="Rekomendowane smartfony" products={smartphones} />
+      <HobbiesSection headerTitle="Rozwijaj swoje pasje" hobbies={hobbies} />
     </div>
   );
 };
