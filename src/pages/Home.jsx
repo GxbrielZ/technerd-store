cbc4e16fba174e6ec5b8a92706cedcda029908b7
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 18:39:48 +0200

    Update Home page with DealsSection component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Hero from "../components/Hero";
 import ProductsSection from "../components/ProductsSection";
 import { newProducts } from "../data/newProducts";
+import DealsSection from "../components/DealsSection";
+import { deals } from "../data/deals";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <ProductsSection headerTitle="Nowe Produkty" products={newProducts} />
+      <DealsSection headerTitle="Oferty i okazje na czasie" deals={deals} />
     </div>
   );
 };
