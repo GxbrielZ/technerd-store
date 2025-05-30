c1eff57beea842b3d00f1a87c927832ac061d061
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-05-30 14:23:04 +0200

    Add image sources

--- src/data/newProducts.js
+++ src/data/newProducts.js
@@ -1,50 +1,59 @@
+import product1 from "../assets/products/newProducts/pralka-lg.jpg";
+import product2 from "../assets/products/newProducts/mikrofala-hisense.jpg";
+import product3 from "../assets/products/newProducts/sony-alpha-aparat.jpg";
+import product4 from "../assets/products/newProducts/hulajnoga-revolution.JPG";
+import product5 from "../assets/products/newProducts/rower-indiana.jpg";
+import product6 from "../assets/products/newProducts/ekspres-siemens.jpg";
+import product7 from "../assets/products/newProducts/smartwatch-garmin.jpg";
+import product8 from "../assets/products/newProducts/nintendo.jpg";
+
 export const newProducts = [
     {
         id: 1,
         name: 'Pralka LG F2W8S506J Vivace Steam AI DD 8kg 1200rpm',
         price: 1998.00,
-        image: "",
+        image: product1,
     },
     {
         id: 2,
         name: 'Kuchenka mikrofalowa HISENSE H23MOBSD1HG 800W Pulse Technology Czarny ',
         price: 499.99,
-        image: "",
+        image: product2,
     },
     {
         id: 3,
         name: 'Aparat bezlusterkowy SONY Alpha ILCE6400LB',
         price: 3980.00,
-        image: "",
+        image: product3,
     },
     {
         id: 4,
         name: 'Hulajnoga elektryczna R.EVOLUTION R-EV42112 20km 350W Czarny',
         price: 1350.00,
-        image: "",
+        image: product4,
     },
     {
         id: 5,
         name: 'Rower górski MTB INDIANA X-Pulser 2.7 M17 Czarno-żółty',
         price: 1299.99,
-        image: "",
+        image: product5,
     },
     {
         id: 6,
         name: 'Ekspres SIEMENS EQ700 TP713R19 Czarny',
         price: 3800.00,
-        image: "",
+        image: product6,
     },
     {
         id: 7,
         name: 'Zegarek sportowy GARMIN Vivoactive 6 Zielony',
         price: 1420.50,
-        image: "",
+        image: product7,
     },
     {
         id: 8,
         name: 'Konsola NINTENDO Switch 2 Czarny + Mario Kart World',
         price: 2229.99,
-        image: "",
+        image: product8,
     },
 ];
