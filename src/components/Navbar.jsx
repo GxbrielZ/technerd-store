404b603afe000df20aeeb51715f60fec76621251
Gabriel Zalewski <gabrielz2702@gmail.com>
2025-06-01 20:59:26 +0200

    Changed logo to Link that can link to home page. Create item quantity circle for cart.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { Link } from "react-router";
 import { FiMenu, FiX, FiShoppingCart, FiHeart, FiUser, FiSearch } from 'react-icons/fi';
+import { useCart } from '../context/CartContext';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { totalItemsInCart } = useCart();
 
   return (
     <nav className="font-roboto bg-primary text-darkwhite py-3 md:py-4">
       <div className="mx-4 md:mx-40 flex items-center justify-between">
         {/* Logo */}
-        <div className="font-ranchers text-2xl md:text-3xl font-bold cursor-pointer">TechNerd</div>
+        <Link to="/" className="font-ranchers text-2xl md:text-3xl font-bold cursor-pointer">TechNerd</Link>
 
         {/* Search Bar */}
         <div className="hidden md:flex flex-1 ml-12 gap-1">
@@ -25,7 +27,14 @@ const Navbar = () => {
 
         {/* Icons */}
         <div className="hidden md:flex gap-6 items-center">
-          <FiShoppingCart size={24} className='cursor-pointer hover:text-lightgrey duration-500' />
+          <Link to="/cart" className="relative cursor-pointer hover:text-lightgrey duration-500">
+            <FiShoppingCart size={24} />
+            {totalItemsInCart > 0 && (
+              <span className="absolute -top-2 -right-2 bg-rose text-darkwhite text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                {totalItemsInCart}
+              </span>
+            )}
+          </Link>
           <FiHeart size={24} className='cursor-pointer hover:text-lightgrey duration-500' />
           <FiUser size={24} className='cursor-pointer hover:text-lightgrey duration-500' />
         </div>
@@ -53,14 +62,22 @@ const Navbar = () => {
             </button>
           </div>
           <div className="flex flex-col gap-4 text-lg my-10">
-            <Link className="border-b-2 w-full pb-2 pl-2">Oferta dla Ciebie</Link>
-            <Link className="border-b-2 w-full pb-2 pl-2">Promocje</Link>
-            <Link className="border-b-2 w-full pb-2 pl-2">Status zamówienia</Link>
-            <Link className="border-b-2 w-full pb-2 pl-2">Gwarancje</Link>
-            <Link className="border-b-2 w-full pb-2 pl-2">Usługi dodatkowe</Link>
+            <Link to="/oferta" className="border-b-2 w-full pb-2 pl-2">Oferta dla Ciebie</Link>
+            <Link to="/promocje" className="border-b-2 w-full pb-2 pl-2">Promocje</Link>
+            <Link to="/status-zamowienia" className="border-b-2 w-full pb-2 pl-2">Status zamówienia</Link>
+            <Link to="/gwarancje" className="border-b-2 w-full pb-2 pl-2">Gwarancje</Link>
+            <Link to="/uslugi-dodatkowe" className="border-b-2 w-full pb-2 pl-2">Usługi dodatkowe</Link>
           </div>
           <div className="flex justify-around text-white text-lg">
-            <FiShoppingCart size={24} />
+            {/* Ikona koszyka w menu mobilnym */}
+            <Link to="/cart" className="relative cursor-pointer">
+              <FiShoppingCart size={24} />
+              {totalItemsInCart > 0 && (
+                <span className="absolute -top-2 -right-2 bg-rose text-darkwhite text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                  {totalItemsInCart}
+                </span>
+              )}
+            </Link>
             <FiHeart size={24} />
             <FiUser size={24} />
           </div>
